5be0ba3521c35bcda3c79c1d3a39a3121ff046f9
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-02 19:51:03 +0530

    [Neeraj] Add. remaining properties to Payroll DTO and Model with validation for Create and Update REST calls

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -10,7 +14,23 @@ public class EmployeeDTO {
     @NotEmpty(message = "Name cannot be empty")
     @Pattern(regexp = "^[A-Z][a-zA-Z\\s]+$", message = "Name should start with uppercase")
     private String name;
+    
+    @NotBlank(message = "Gender cannot be empty")
+    private String gender;
 
-    @Min(value = 1000, message = "Salary should be at least 5000")
+    @PastOrPresent(message = "Start date cannot be in the future")
+    @JsonFormat(pattern = "dd MMM yyyy")
+    private LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be empty")
+    private String note;
+
+    @NotBlank(message = "Profile picture cannot be empty")
+    private String profilePic;
+
+    @NotBlank(message = "Department cannot be empty")
+    private String department;
+
+    @Min(value = 5000, message = "Salary should be at least 5000")
     private Double salary;
 }

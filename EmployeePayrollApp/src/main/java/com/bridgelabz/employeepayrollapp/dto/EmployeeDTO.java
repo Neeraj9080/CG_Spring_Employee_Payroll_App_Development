db251f79c28d625cf5fe9a7bc13143d5eb7ebe91
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-02 18:05:37 +0530

    [Neeraj] Add. Lombok support for getters and setters

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -3,39 +3,14 @@ package com.bridgelabz.employeepayrollapp.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
+@Data
 public class EmployeeDTO {
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]+$", message = "Name should start with uppercase")
+    private String name;
 
-	@NotEmpty(message = "Name cannot be empty")
-	@Pattern(regexp = "^[A-Z][a-zA-Z\\s]+$", message = "Name should start with uppercase")
-	private String name;
-	
-	@Min(value = 5000, message = "Salary should be at least 5000")
+    @Min(value = 5000, message = "Salary should be at least 5000")
     private Double salary;
-	
-	public EmployeeDTO() {
-	}
-	
-	public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-	
-	// Getters and Setters
-	
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
